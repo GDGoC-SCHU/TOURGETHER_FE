3bd7d32ea51c395dac6cbf3b94c356cddf4070f7
apple <apple@apples-MacBook-Air.local>
2025-05-11 19:36:29 +0900

    [feat] Home 화면 생성

---
+++ layouts/NavBar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "@/styles/ViewStyle";
+import Feather from "@expo/vector-icons/Feather";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+export default function NavBar() {
+  return (
+    <View style={styles.NavBarContainer}>
+      <View style={styles.NavBar}>
+        <View style={styles.NavBarIcon}>
+          <Feather name="home" size={24} color="black" />
+          <Text style={styles.NavBarText}>홈</Text>
+        </View>
+        <View style={styles.NavBarIcon}>
+          <MaterialIcons name="chat-bubble-outline" size={24} color="black" />
+          <Text style={styles.NavBarText}>게시판</Text>
+        </View>
+        <View style={styles.NavBarIcon}>
+          <MaterialCommunityIcons name="chat-outline" size={24} color="black" />
+          <Text style={styles.NavBarText}>채팅</Text>
+        </View>
+        <View style={styles.NavBarIcon}>
+          <FontAwesome name="user-circle-o" size={24} color="black" />
+          <Text style={styles.NavBarText}>프로필</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
--- styles/ViewStyle.ts
+++ styles/ViewStyle.ts
@@ -15,4 +15,57 @@ export const styles = StyleSheet.create({
     height: 1,
     width: "80%",
   },
-});
+  NavBarContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "#fff",
+    paddingVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    zIndex: 10,
+  },
+  NavBar: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    marginRight: 20,
+  },
+  NavBarIcon: {
+    justifyContent:"space-evenly",
+    width: 30,
+    height: 30,
+    marginBottom: 20,
+    marginLeft: 45,
+    marginRight: 20,
+  },
+  NavBarText: {
+    justifyContent:"space-evenly",
+    marginRight: 5,
+    textAlign: "center",
+    fontSize: 8,
+    fontWeight: "bold",
+    color: "black",
+  },
+  headerContainer: {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  flexDirection: "row",
+  justifyContent: "space-between", 
+  alignItems: "center",           
+  paddingHorizontal: 20,           
+  paddingTop: 20,                  
+},
+headerIcon: {
+  width: 30,
+  height: 30,
+},
+
+}); 
+
